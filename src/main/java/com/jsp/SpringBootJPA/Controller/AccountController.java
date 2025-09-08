ce6a8be70c56e3692e54ac569c8f6e5fa9952d3e
salma shaik <salmashaik63108@gmail.com>
2025-09-08 11:45:29 +0530

    change the restapis

--- src/main/java/com/jsp/SpringBootJPA/Controller/AccountController.java
+++ src/main/java/com/jsp/SpringBootJPA/Controller/AccountController.java
@@ -28,7 +28,7 @@ public class AccountController {
 	@Autowired
 	private AccountService accountservice;
 	
-	@PostMapping("/saving")
+	@PostMapping("/save")
 	public ResponseEntity<Accounts> saveaccount(@RequestBody Accounts account) {
 		
 		Accounts addaccount = accountservice.addaccount(account);
